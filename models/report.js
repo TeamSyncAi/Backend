ee648e1dfe4c037a1a0ec671a9896da1515d4bc0
Ekbel <zrellliekbel@gmail.com$>
2024-02-21 06:58:18 +0100

    model report

--- models/report.js
+++ models/report.js
@@ -3,9 +3,30 @@ const { Schema, model, Types } = mongoose;
 
 const reportSchema = new Schema({
     titre: {
-    type: String,
-    required: true
-  },
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    dateDebut: {
+        type: Date,
+        required: true
+    },
+    dateFin: {
+        type: Date,
+        required: true
+    },
+   /* createur: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Utilisateur',
+        required: true
+    },*/
+    donnees: {
+        type: Object, 
+        required: true
+    }
 },
 {
     timestamps : true
