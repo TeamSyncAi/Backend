adb93ea9dd8c91d10d78b4ee2f415122681426a7
Ekbel <zrellliekbel@gmail.com$>
2024-02-21 06:12:05 +0100

    getOnce done

--- controllers/reclamationController.js
+++ controllers/reclamationController.js
@@ -43,4 +43,14 @@ export function deleteAllReclamation(req, res) {
     });
 }
 
+export function getOnceReclamation(req, res) {
+  reclamation.findOne({ _id: req.params._id })
+    .then((docs) => {
+      res.status(200).json(docs);
+    })
+    .catch((err) => {
+      res.status(500).json({ error: err });
+    })
+}
+
 
